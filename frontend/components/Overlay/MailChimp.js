e6a5dd658592a111e26c3989c6f197dba9789f86
Miguel Angel Contreras <miguelangelcontrerascruz@gmail.com>
2022-12-21 00:08:06 -0600

    Estilos creados || Falta feedback del UI
    Solo hace falta agregar una retroalimentación en el modal de registro exitoso

--- frontend/components/Overlay/MailChimp.js
+++ frontend/components/Overlay/MailChimp.js
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useContext } from "react";
 import Backdrop from "./Backdrop";
 import Modal from "./Modal";
+import classes from "../../styles/Form.module.css";
+import { ModalContext } from "../../store/modal-context";
 
 export default function MailChimp() {
+  const modalctx = useContext(ModalContext);
   const [radioValue, setRadioValue] = useState();
   const emailRef = useRef(null);
   const firstNameRef = useRef(null);
@@ -11,6 +14,7 @@ export default function MailChimp() {
 
   const subscribeUser = async (e) => {
     e.preventDefault();
+    modalctx.mountHandler();
     try {
       const res = await fetch("api/subscribeUser", {
         body: JSON.stringify({
@@ -33,24 +37,14 @@ export default function MailChimp() {
   return (
     <>
       <Backdrop />
-      <Modal>
-        <div className="">
-          <div className="">
-            <h1 className="">Subscribete </h1>
-            <h4>¿Estás interesadx?</h4>
-          </div>
-          <div className="">
-            <form onSubmit={subscribeUser}>
-              <label for="email-input">Pon tu email</label>
-              <input
-                type="email"
-                id="email-input"
-                name="email"
-                placeholder="Email aquí"
-                ref={emailRef}
-                required
-              />
-              <label for="firstName-input">Pon tu Nombre</label>
+      <Modal styleModal={classes.modal}>
+        <div className={classes.container}>
+          <h4 className={classes.titulo}>¿Estás interesadx?</h4>
+          <div className={classes.container}>
+            <form onSubmit={subscribeUser} className={classes.form}>
+              <label className={classes.label} for="firstName-input">
+                Nombre
+              </label>
               <input
                 type="text"
                 id="firstName-input"
@@ -58,37 +52,71 @@ export default function MailChimp() {
                 placeholder="Nombre aquí"
                 ref={firstNameRef}
                 required
+                className={classes.field}
               />
-              <h5>Me interesa... *</h5>
-
-              <label for="radio1">Typefobia en línea</label>
+              <label className={classes.label} for="email-input">
+                Email
+              </label>
               <input
-                type="radio"
-                id="radio1"
-                name="radio"
-                value="Typefobia en línea"
-                onClick={handleSettingsChange}
-              ></input>
-              <label for="radio2">Typefobia presencial</label>
-              <input
-                type="radio"
-                id="radio2"
-                name="radio"
-                value="Typefobia presencial"
-                onClick={handleSettingsChange}
-              ></input>
-              <label for="radio3">Ambos</label>
-              <input
-                type="radio"
-                id="radio3"
-                name="radio"
-                value="Ambos"
-                onClick={handleSettingsChange}
-              ></input>
+                className={classes.field}
+                type="email"
+                id="email-input"
+                name="email"
+                placeholder="Email aquí"
+                ref={emailRef}
+                required
+              />
+              <h5 className={classes.subtitulo}>Me interesa... *</h5>
+              <div className={classes.radio}>
+                <input
+                  type="radio"
+                  id="radio1"
+                  name="radio"
+                  value="Typefobia en línea"
+                  onClick={handleSettingsChange}
+                ></input>
+                <label className={classes.label} for="radio1">
+                  Typefobia en línea
+                </label>
+              </div>
+              <div className={classes.radio}>
+                <input
+                  type="radio"
+                  id="radio2"
+                  name="radio"
+                  value="Typefobia presencial"
+                  onClick={handleSettingsChange}
+                ></input>
+                <label className={classes.label} for="radio2">
+                  Typefobia presencial
+                </label>
+              </div>
+              <div className={classes.radio}>
+                <input
+                  type="radio"
+                  id="radio3"
+                  name="radio"
+                  value="Ambos"
+                  onClick={handleSettingsChange}
+                ></input>
+                <label className={classes.label} for="radio3">
+                  Ambos
+                </label>
+              </div>
               <div className="flex justify-center items-center">
-                <button type="submit" value="" name="subscribe" className="">
-                  Subscribe
+                <button
+                  type="submit"
+                  value=""
+                  name="subscribe"
+                  className={classes.enviar}
+                >
+                  ¡Pre-Registrate!
                 </button>
+                <p className={classes.info}>
+                  Typefobia utilizará estos datos para estimar las personas que
+                  estén interesadxs en asistir al anti-congreso y tomar las
+                  mejores decisiones.
+                </p>
               </div>
             </form>
           </div>
