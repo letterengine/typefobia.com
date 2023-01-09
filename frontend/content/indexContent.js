3e209190803107505fb8ffa864e844a2413c18cb
eduairet <hola@eduairet.com>
2023-01-09 01:05:02 -0600

    Responsive problems fixed

--- frontend/content/indexContent.js
+++ frontend/content/indexContent.js
@@ -27,7 +27,7 @@ const indexContent = {
     ),
     musaMap: {
         src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4847572484755!2d-98.1928851!3d19.0424128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc0dd11a2c8a5%3A0x7fcf0493a0d8b004!2sMUSA%20Cultura%20Visual!5e0!3m2!1ses-419!2smx!4v1673241713008!5m2!1ses-419!2smx',
-        width: '600',
+        width: '100%',
         height: '450',
         style: { border: 0 },
         allowFullScreen: '',
