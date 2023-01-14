8feb68ea13fd142217d4afa2bd8e52f108fce671
eduairet <hola@eduairet.com>
2023-01-13 21:16:27 -0600

    Border footer

--- frontend/pages/talleres.js
+++ frontend/pages/talleres.js
@@ -3,15 +3,22 @@ import MainContainer from '@components/Containers/MainContainer';
 import Section from '@components/Containers/Section';
 import Container from '@components/Containers/Container';
 import Taller from '@components/Layout/Taller';
+import SubNav from '@components/UI/SubNav';
+
+const subNavItems = [
+    { href: '#virtuales', label: 'Virtuales' },
+    { href: '#presenciales', label: 'Presenciales' },
+];
 
 export default function Talleres() {
     return (
         <>
             <MainContainer>
                 <h1>Talleres</h1>
+                <SubNav id='subnav-talleres' links={subNavItems} />
             </MainContainer>
             <Section id='talleres'>
-                <Container>
+                <Container id='virtuales'>
                     <h2>Virtuales</h2>
                     {Object.values(talleresContent.virtuales).map(
                         (taller, i) => (
@@ -19,7 +26,7 @@ export default function Talleres() {
                         )
                     )}
                 </Container>
-                <Container>
+                <Container id='presenciales'>
                     <h2>Presenciales</h2>
                     {Object.values(talleresContent.presenciales).map(
                         (taller, i) => (
